32715fb8aec4a3d4ba3061e691ec2a0b8513ab23
Prashid Pradhan <stropher@gmail.com>
2016-12-13 16:48:37 +0545

    Initial Commit

---
+++ BugTracker/BugTracker/Adminlogin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugTracker
+{
+    public partial class Adminlogin : Form
+    {
+        DatabaseConnection conn = new DatabaseConnection();
+        public Adminlogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String query;
+            query = " Select username from Admin WHERE username ='"+textUname.Text+"'AND password ='"+textPwd.Text+"'";
+                DataTable dt = conn.retrieve(query);
+            if (dt.Rows.Count>0)
+            {
+                MessageBox.Show("Welcome to Dashboard");
+            }
+            else
+            {
+                MessageBox.Show("Either Username or Password is Incorrect");
+            }
+
+        }
+
+        private void textUname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textPwd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BugTracker/BugTracker/DatabaseConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker
+{
+    class DatabaseConnection
+    {
+        SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Temp\BugtrackerDB.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        // For insert update and delete Statement
+        public void manipulate(string query)
+        {
+            cn.Open();
+            cmd.Connection = cn;
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+
+        // For Select Statement
+        public DataTable retrieve(string query)
+        {
+            DataSet ds = new DataSet();
+            da = new SqlDataAdapter(query, cn);
+            da.Fill(ds);
+            return ds.Tables[0];
+
+        }
+
+    }
+}
